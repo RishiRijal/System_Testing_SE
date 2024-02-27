38266ee083a48490cdc419dbe55402ce03e00d58
RishiRijal <135732124+RishiRijal@users.noreply.github.com>
2024-02-27 09:24:24 +0200

    Update sensors_tests.py

--- sensors_tests.py
+++ sensors_tests.py
@@ -1,13 +1,23 @@
 import sensors_main
 import unittest
-import sys # needed for setting the command line parameters for test cases
-from unittest.mock import patch # needed for the integration test case
-
+from io import StringIO
+import sys
 
 class TestSystem(unittest.TestCase):
-    def test_start_sensors_main_with_incorrect_arguments(self):
-        result = subprocess.run(['python', 'sensors_main.py', '22', '18'], capture_output=True, text=True)
-        self.assertIn('Error: Incorrect command line arguments.', result.stdout)
+
+    def test_system_main(self):
+        sys.argv = ['sensors_main.py', '16', '16']
+        expected_output = "Error: Incorrect command line arguments.\n"
+        
+        sys.stdout = StringIO()
+        
+        sensors_main.main()
+        output = sys.stdout.getvalue()
+
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(output, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
+
